e199f1b56c2469d74aeee98939aef4be5a21060b
CXT <cxt@CXTdeMacBook-Pro.local>
2017-10-06 16:08:03 -0700

    add exam

--- webDev/js-basic/word-match.js
+++ webDev/js-basic/word-match.js
@@ -6,12 +6,12 @@ const match = (first, second) => {
 	const secondLowerCase = second.toLowerCase();
 
 	let hash = [];
-	for (let i = 0; i < firstLowerCase.length - 1; i++) {
+	for (let i = 0; i < firstLowerCase.length; i++) {
 		hash[firstLowerCase.charAt(i) - 'a']++;
 	}
 
 	let count = 0;
-	for (let i = 0; i < secondLowerCase.length - 1; i++) {
+	for (let i = 0; i < secondLowerCase.length; i++) {
 		if (hash[secondLowerCase.charAt(i) - 'a'] > 0) {
 			count++;
 			hash[secondLowerCase.charAt(i) - 'a'] -= 1;
